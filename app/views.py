4ba06efe9f025b3092e508bb1f19bdb900defcc2
subham31p <subhampatro3102@gmail.com>
2023-11-08 16:40:49 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import *
+# Create your views here.
+def subham(request):
+    return HttpResponse(' <h1><marquee>hi jhampuri  </h1></marquee>')
